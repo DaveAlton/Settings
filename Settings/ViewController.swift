a12f6d6973e3380749afadd7bf8fb2f811245784
Dave Alton <dave@ovuline.com>
2018-11-10 12:50:49 -0800

    Fixed bugs by switching to Enums
    By switching to enums, we can dynamically add and remove cells, and configure those cells easier and cleaner while also fixing bugs.

--- Settings/ViewController.swift
+++ Settings/ViewController.swift
@@ -8,14 +8,61 @@
 
 import UIKit
 
+enum Settings {
+    case awesomeMode, faceID, website, appVersion
+
+    var reuseIdentifier: String {
+        switch self {
+        case .awesomeMode: return "SwitchCell"
+        case .faceID:      return "SwitchCell"
+        case .website:     return "LabelCell"
+        case .appVersion:  return "LabelCell"
+        }
+    }
+
+    var userInteractionEnabled: Bool {
+        switch self {
+        case .awesomeMode: return true
+        case .faceID:      return true
+        case .website:     return true
+        case .appVersion:  return false
+        }
+    }
+
+    var label: String {
+        switch self {
+        case .awesomeMode: return "Awesome Mode"
+        case .faceID:      return "FaceID"
+        case .website:     return "Expedia.com"
+        case .appVersion:  return "App Version: 2.0"
+        }
+    }
+
+    var accessoryType: UITableViewCell.AccessoryType {
+        switch self {
+        case .awesomeMode: return .none
+        case .faceID:      return .none
+        case .website:     return .disclosureIndicator
+        case .appVersion:  return .none
+        }
+    }
+}
+
 class ViewController: UIViewController {
     @IBOutlet var tableView: UITableView!
     var awesomeMode = false
     var faceIDAvailable = true
     var faceIDOn = false
+    var settings = [Settings]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        settings = [.awesomeMode]
+        if faceIDAvailable {
+            settings += [.faceID]
+        }
+        settings += [.website, .appVersion]
 
         tableView.reloadData()
     }
@@ -39,45 +86,32 @@ class ViewController: UIViewController {
 
 extension ViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        var cells = 3
-        if faceIDAvailable {
-            cells += 1
-        }
-        return cells
+        return settings.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if indexPath.row == 0 {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "SwitchCell", for: indexPath) as! SwitchCell
-            cell.switchHandler = awesomeSwitchTapped
-            cell.isUserInteractionEnabled = true
-            cell.`switch`.setOn(awesomeMode, animated: false)
-            cell.label.text = "Awesome Mode"
-            return cell
+        let setting = settings[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: setting.reuseIdentifier, for: indexPath)
+        cell.isUserInteractionEnabled = setting.userInteractionEnabled
+        cell.accessoryType = setting.accessoryType
+        (cell as? SwitchCell)?.label.text = setting.label
+        (cell as? LabelCell)?.label.text = setting.label
+
+        if setting == .awesomeMode {
+            (cell as? SwitchCell)?.switchHandler = awesomeSwitchTapped
+            (cell as? SwitchCell)?.`switch`.setOn(awesomeMode, animated: false)
         } else if (faceIDAvailable && indexPath.row == 1) {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "SwitchCell", for: indexPath) as! SwitchCell
-            cell.switchHandler = faceIDSwitchTapped
-            cell.isUserInteractionEnabled = true
-            cell.`switch`.setOn(faceIDOn, animated: false)
-            cell.label.text = "Use FaceID"
-            return cell
-        } else if (!faceIDAvailable && indexPath.row == 1) || (faceIDAvailable && indexPath.row == 2) {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "LabelCell", for: indexPath) as! LabelCell
-            cell.isUserInteractionEnabled = true
-            cell.accessoryType = .disclosureIndicator
-            cell.label.text = "Expedia.com"
-            return cell
-        } else {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "LabelCell", for: indexPath) as! LabelCell
-            cell.isUserInteractionEnabled = false
-            cell.label.text = "App Version: 1.0"
-            return cell
+            (cell as? SwitchCell)?.switchHandler = faceIDSwitchTapped
+            (cell as? SwitchCell)?.`switch`.setOn(faceIDOn, animated: false)
         }
+
+        return cell
     }
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: false)
-        if indexPath.row == 1 {
+        let setting = settings[indexPath.row]
+        if setting == .website {
             UIApplication.shared.open(URL(string: "https://www.expedia.com")!, options: [:], completionHandler: nil)
         }
     }
